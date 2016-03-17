ab2f5195a8779bd949f13a5459657f21d73c83b0
Jesse Shawl <jesse@jshawl.com>
2016-03-17 15:59:13 -0400

    show the thangs

--- app.js
+++ app.js
@@ -8,11 +8,26 @@ app.use(express.static('public'));
 
 mongoose.connect("mongodb://localhost/admissions");
 
+var Submission = require("./models/submission")(mongoose);
 
 app.use(bodyParser.urlencoded())
 app.set("view engine","hbs");
 app.get("/", function(req, res){
   res.render("index")
+})
+
+app.get("/insecure", function(req, res){
+  Submission.find({}, function(err, submissions){
+    res.render("admin",{submissions: submissions})
+  })
+})
+
+app.get("/:id", function(req, res){
+  Submission.findOne({_id: req.params.id}, function(err, doc){
+    console.log(doc)
+    var blob = doc.tests[doc.tests.length - 1].blob
+    res.render("index", {blob: blob})
+  })
 })
 
 app.listen(3000)
